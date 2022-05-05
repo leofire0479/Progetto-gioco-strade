b63265a4afdda00f1d4773dd3bfca66645263764
leofire0479 <leonardo.cappello04@gmail.com>
2022-05-05 19:30:26 +0200

    First commit

---
+++ Progetto Gioco con Strade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Progetto Gioco con Strade
+//
+//  Created by Leonardo Cappello on 05/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
